7bccf56d2e5d5ded8bcd9978169e1152fc5a8093
midorigame737 <midorigame6@gmail.com>
2022-09-29 18:30:06 +0900

    外部キーとかでエラー起きてたので修正

--- database/init/create_tables.sql
+++ database/init/create_tables.sql
@@ -1,9 +1,10 @@
 CREATE DATABASE memberssite_db;
-CREATE TABLE  memberssite_db.department(
+use memberssite_db;
+CREATE TABLE  department(
     department_id int NOT NULL PRIMARY KEY,
     depart_name varchar(256),
     member_id int );
-CREATE TABLE  memberssite_db.member_table(
+CREATE TABLE  member_table(
     student_id int auto_increment PRIMARY KEY,
     member_name varchar(256),
     pass varchar(128),
@@ -11,24 +12,28 @@ CREATE TABLE  memberssite_db.member_table(
     department_id int,
     INDEX DepartmentIndex(department_id),
     FOREIGN KEY fk_department(department_id)
-    REFERENCES memberssite_db.department(department_id));
-CREATE TABLE memberssite_db.Member_Language(
+    REFERENCES department(department_id));
+CREATE TABLE Member_Language(
     StudentId int,
     INDEX LanguageIndex(StudentId),
-    FOREIGN KEY fk_LanguageId(StudentId) REFERENCES memberssite_db.member(student_Id)
+    FOREIGN KEY fk_LanguageId(StudentId) REFERENCES member_table(student_Id)
 );
-CREATE TABLE memberssite_db.Member_Project(
+CREATE TABLE Member_Project(
     StudentId int,
     LanguageID int,
     skill varchar(128),
     INDEX LanguageIndex(StudentId),
-    FOREIGN KEY fk_LanguageId(StudentId) REFERENCES memberssite_db.member(student_id)
+    FOREIGN KEY fk_LanguageId(StudentId) REFERENCES member_table(student_id)
 );
 create table Project(
 	id int primary key,
     Project_name varchar(128),
     created date
     );
+CREATE TABLE LanguageTable(
+	LanguageId int NOT NULL PRIMARY KEY,
+    LanguageName varchar(128)
+);
 create table Project_Language(
 	LanguageId int,
     index Lid(LanguageId),
@@ -39,13 +44,3 @@ create table Project_Language(
     foreign key (ProjectId)
     references Project(id) 
     );
-    
-CREATE TABLE memberssite_db.LanguageTable(
-	LanguageId int NOT NULL PRIMARY KEY,
-    LanguageName varchar(128)
-);
-create table memberssite_db.Member_Language(
-	id int primary key,
-    Language_name varchar(64)
-    );
-
